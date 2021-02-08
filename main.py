41d6455e93f96cbcb808f57e6ee8f4a160b16ae6
n-rule <rule@ukr.net>
2021-02-08 19:33:44 +0200

    1

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from random import *
 from tkinter import *
 from tkinter import messagebox
+
 import pyperclip
-from random import *
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
