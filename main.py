0c80e2bb8d65ffb33386377b5c2e98b56b5f4797
n-rule <rule@ukr.net>
2021-02-08 17:59:15 +0200

    1

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from tkinter import *
 from tkinter import messagebox
 import pyperclip
-
+from random import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     # Password Generator Project
-    import random
+
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
-    password_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
-    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
 
     password_list = password_letters + password_numbers + password_symbols
-    random.shuffle(password_list)
+    shuffle(password_list)
     password = ''.join(password_list)
     pyperclip.copy(password)
 
